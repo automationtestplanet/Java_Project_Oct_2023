d59858fcf1824ad5274bbd5d2d8d95d66679eef7
RAJU CHELLE <rajuchelle@gmail.com>
2023-10-21 08:06:09 +0530

    new 1234 changes

--- src/encapsulation/TypeConversion.java
+++ src/encapsulation/TypeConversion.java
@@ -3,8 +3,7 @@ package encapsulation;
 public class TypeConversion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("This is Type Conversion from 1234 ");
 	}
 
 }
