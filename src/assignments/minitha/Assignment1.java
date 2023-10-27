803c140341a638a3040a6aa4ea6052107df3becd
Minithchandragiri <minithachndragiri143@gmil.com>
2023-10-27 22:32:16 +0530

    completed

--- src/assignments/minitha/Assignment1.java
+++ src/assignments/minitha/Assignment1.java
@@ -5,8 +5,18 @@ public class Assignment1 {
 	// Declare an Array and print only even numbers form the array
 
 	public static void main(String[] args) {
+		int[] array = { 1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("Even numbers in the array ");
+		for(int i=0; i<array.length; i++) {
+			if (array[i]%2 == 0) {
+				System.out.println(array[i]);
+			}
+		}
+			}
+		}
+		
+		
 		
 
-	}
-
-}
+	
