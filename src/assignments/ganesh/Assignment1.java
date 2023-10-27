6b4b1970f5512f16a8d1ec3ed61662553d5acb0c
ganeshgit4 <badeganesh69@gmail.com>
2023-10-27 21:26:48 +0530

    Assignment by Ganesh

--- src/assignments/ganesh/Assignment1.java
+++ src/assignments/ganesh/Assignment1.java
@@ -5,7 +5,10 @@ public class Assignment1 {
 	// Declare an Array and print only even numbers form the array
 
 	public static void main(String[] args) {
-		
+		int[] array1 = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+		for(int i=0;i<array1.length;i++)
+	if(array1[i]%2==0)
+				System.out.println("Even numbers from the array are:"+array1[i]);
 
 	}
 
