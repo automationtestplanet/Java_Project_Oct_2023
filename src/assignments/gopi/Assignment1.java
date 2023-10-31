e60c071619613c0971b08e6e916cd3f40c91d0a2
RAJU CHELLE <rajuchelle@gmail.com>
2023-10-31 07:07:11 +0530

    code reviw changes

--- src/assignments/gopi/Assignment1.java
+++ src/assignments/gopi/Assignment1.java
@@ -1,30 +1,21 @@
 package assignments.gopi;
 
 public class Assignment1 {
-	
+
 	// Declare an Array and print only even numbers form the array
 
-	public static void main(String[] args) {
-		int[] arr= new int[10];
-		arr[0]=1;
-		arr[1]=2;
-		arr[2]=3;
-		arr[3]=4;
-		arr[4]=5;
-		arr[5]=6;
-		arr[6]=7;
-		arr[7]=8;
-		arr[8]=9;
-		arr[9]=10;
-    for(int i=0;i<arr.length;i++) {
-    	if(arr[i]%2==0) {
-    		System.out.println("Even numbers are :"+arr[i]);
-    	}
-    	/*else {
-    		System.out.println("remaining numbers are  not a even numbers");
-    	}*/
-    }
+	static void displayEvenNumbersFromArray(int[] intArr) {
+		for (int eahElelement : intArr) {
+			if (eahElelement % 2 == 0) {
+				System.out.println(eahElelement + " : is a Even number");
+			}
+		}
+	}
 
+	public static void main(String[] args) {
+
+		int[] intArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		displayEvenNumbersFromArray(intArr);
 	}
 
 }
