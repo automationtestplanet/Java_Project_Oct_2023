b028b65b8667c3fcf7f6e1058ca6606f76cdd57f
ganeshgit4 <badeganesh69@gmail.com>
2023-11-01 10:06:05 +0530

    Assignment resubmission by ganesh

--- src/assignments/ganesh/Assignment1.java
+++ src/assignments/ganesh/Assignment1.java
@@ -7,7 +7,7 @@ public class Assignment1 {
 		int[] array1 = {0,1,2,3,4,5,6,7,8,9,10,11,12};
 		for(int i=0;i<array1.length;i++)
 	if(array1[i]%2==0)
-				System.out.println("Even numbers from the array are:"+array1[i]);
+				System.out.println("Even numbers from the array are :"+array1[i]);
 
 	}
 
