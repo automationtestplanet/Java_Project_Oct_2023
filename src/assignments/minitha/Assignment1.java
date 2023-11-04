242e8c6c5727a9572568c16f38288c05fc811325
Minithchandragiri <minithachndragiri143@gmil.com>
2023-11-04 08:00:26 +0530

    assignment 1 changes

--- src/assignments/minitha/Assignment1.java
+++ src/assignments/minitha/Assignment1.java
@@ -1,22 +1,23 @@
 package assignments.minitha;
 
 public class Assignment1 {
-	
+
 	// Declare an Array and print only even numbers form the array
 
-	public static void main(String[] args) {
-		int[] array = { 1,2,3,4,5,6,7,8,9,10};
-		
-		System.out.println("Even numbers in the array ");
-		for(int i=0; i<array.length; i++) {
-			if (array[i]%2 == 0) {
-				System.out.println("Even number is "+array[i]);
+	static void displayEvenNumbersFromArray(int[] intArr) {
+		for (int eachElement : intArr) {
+			if (eachElement % 2 == 0) {
+				System.out.println(eachElement + ":is a Even number");
 			}
 		}
-			}
-		}
-		
-		
-		
 
-	
+	}
+
+	public static void main(String[] args) {
+
+		int[] intArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		displayEvenNumbersFromArray(intArr);
+
+	}
+
+}
