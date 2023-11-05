42cade69d2544562b5879d1020ba867b5aa7cbb6
unknown <vsujankumarreddy@gmail.com>
2023-11-05 23:14:13 +0530

    assignment1 changes

--- src/assignments/sujan/Assignment1.java
+++ src/assignments/sujan/Assignment1.java
@@ -4,13 +4,17 @@ public class Assignment1 {
 
 	// Declare an Array and print only even numbers form the array
 
-	public static void main(String[] args) {
-		int[] a = { 3, 6, 7, 22, 9, 5, 2, 8, 11, 23, 17, 10 };
+	static void Display(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			{
 				if (a[i] % 2 == 0)
-					System.out.println(a[i]);
+					System.out.println("even number:" + a[i]);
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 3, 6, 7, 22, 9, 5, 2 };
+		Display(a);
 	}
 }
