b453b381fe356f1f222975ef3c2c05e15d3fefcb
unknown <vsujankumarreddy@gmail.com>
2023-11-11 21:48:30 +0530

    new changes

--- src/assignments/sujan/Assignment2.java
+++ src/assignments/sujan/Assignment2.java
@@ -3,34 +3,29 @@ package assignments.sujan;
 public class Assignment2 {
 
 	// Create a reusable method to check whether the number is Armstrong number not
-	
-	// 153 -> 1 + 125 + 27 = 153  
-	
-	static int z;
-	static int temp = z;
-	int sum = 0;
-	int y;
 
-	void DisplayName() {
+	// 153 -> 1 + 125 + 27 = 153
 
+	void CheckArmstrongNumber(int z) {
+		int temp = z;
+		int sum = 0;
+		int y;
+		System.out.println("Actual given number: " + z);
 		while (z > 0) {
 			y = z % 10;
 			z = z / 10;
 			sum = sum + y * y * y;
 		}
 		if (temp == sum) {
-			System.out.println("the number an armstrong number :" + sum);
+			System.out.println("The given number an armstrong number :" + sum);
 		} else {
-			System.out.println("the number not an armstrong number :" + sum);
+			System.out.println("The given number not an armstrong number :" + sum);
 		}
 	}
 
 	public static void main(String[] args) {
-
-		Assignment2 ass = new Assignment2();
-		ass.z = 153;
-		ass.temp = z;
-		ass.DisplayName();
+		Assignment2 assig = new Assignment2();
+		assig.CheckArmstrongNumber(153);
+		assig.CheckArmstrongNumber(133);
 	}
-
 }
