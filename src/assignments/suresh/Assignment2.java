f96e38392bbde0629e61a306c37ca57d0e563354
RAJU CHELLE <rajuchelle@gmail.com>
2023-11-11 12:39:04 +0530

    suresh aSSIGNMENT

--- src/assignments/suresh/Assignment2.java
+++ src/assignments/suresh/Assignment2.java
@@ -9,8 +9,13 @@ public class Assignment2 {
 	
 	// 13 14 27 41 68
 	
+	static void displayValue(int number) {
+		System.out.println(number);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		displayValue(1000);
 
 	}
 
